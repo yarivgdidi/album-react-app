f34c22e262bf02454b182af9cb17442751be6552
Yariv <yariv.gdidi@gmail.com>
2021-08-09 02:59:56 +0300

    heroku fix

--- src/app/features/album/axiosApi.ts
+++ src/app/features/album/axiosApi.ts
@@ -1,10 +1,11 @@
 import { AlbumApi, FavoriteApi } from '../../../openApiClient';
 import axios from 'axios'
 import { AxiosInstance } from 'axios';
+import {BASE_PATH} from '../../../openApiClient/base'
 
 const axiosInstance: AxiosInstance = axios.create();
-const albumApi = new AlbumApi(undefined, 'http://localhost:3000/api', axiosInstance )
-const favoriteApi = new FavoriteApi(undefined, 'http://localhost:3000/api', axiosInstance )
+const albumApi = new AlbumApi(undefined, BASE_PATH, axiosInstance )
+const favoriteApi = new FavoriteApi(undefined, BASE_PATH , axiosInstance )
 
 export {
   albumApi,

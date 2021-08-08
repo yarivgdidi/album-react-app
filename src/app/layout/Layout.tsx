ed96b6778ca4437b587c8f8dc095b9a5b14092e8
Yariv <yariv.gdidi@gmail.com>
2021-08-08 19:19:08 +0300

    tabs with route

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,21 +1,43 @@
+import {
+    BrowserRouter as Router,
+    Route,
+    NavLink,
+    Switch
+  } from "react-router-dom";
 import { Tabs } from 'antd';
 import { Album }  from '../../features/album/Album'
+
 import './layout.css';
 const { TabPane } = Tabs;
 function Layout(){
     return (
-            <Tabs defaultActiveKey="albums" centered >
-                <TabPane tab="Albums" key="albums">
-                    <div className='container'>
-                        <Album />
-                    </div>    
-                </TabPane>
-                <TabPane tab="Favorites" key="favorites">
-                    <div className='container'>
-                        Content of Tab Pane 2
-                    </div>
-                </TabPane>
-            </Tabs>
+        <Router>
+            <Switch>
+                <Route 
+                path="/:tab?"
+                render={({ match, history }) => {
+                    return (
+                        <Tabs   centered
+                                defaultActiveKey={match.params.tab}
+                                onChange={key => { history.push(`/${key}`);}}
+                        >
+                        <TabPane tab="Albums" key="albums">
+                            <div className='container'>
+                                <Album />
+                            </div>    
+                        </TabPane>
+                        <TabPane tab="Favorites" key="favorites">
+                            <div className='container'>
+                                Content of Tab Pane 2
+                            </div>
+                        </TabPane>
+                    </Tabs>
+                    )
+                }}
+                />
+          </Switch>
+        </Router>
+           
     )
 }
 
